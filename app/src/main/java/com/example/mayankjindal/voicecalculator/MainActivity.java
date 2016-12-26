a96723b66c1750231baf88e876af49b77a2bb5b7
mayankjindal <mayank.jindal5@gmail.com>
2016-12-26 16:38:25 +0530

    project build

--- app/src/main/java/com/example/mayankjindal/voicecalculator/MainActivity.java
+++ app/src/main/java/com/example/mayankjindal/voicecalculator/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
