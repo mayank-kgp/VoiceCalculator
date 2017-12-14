2fac9f0d2e40833d4517c3ad6b9712fad3fe2fcf
Ishu Dohare <ishu.dohare.ece15@itbhu.ac.in>
2017-12-15 03:17:13 +0530

    Settings button removed
    the settings button  issue is fixed.

--- app/src/main/java/com/example/mayankjindal/voicecalculator/MainActivity.java
+++ app/src/main/java/com/example/mayankjindal/voicecalculator/MainActivity.java
@@ -253,13 +253,7 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        return false;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
